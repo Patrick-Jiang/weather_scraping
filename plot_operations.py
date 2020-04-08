b02dbe8b379edb0eef17c0b4223c85e4fb8c4431
patrickjiang <patrick.jiang86@gmail.com>
2020-04-08 13:15:43 -0500

    Added mathplotlib

--- plot_operations.py
+++ plot_operations.py
@@ -10,88 +10,34 @@ class PlotOperations():
     self.weather_data = dict()
     self.month = 0
 
-  def create_weather_data(self,self.start_year,self.end_year):
+  def create_weather_data(self,start_year=0,end_year=0):
+    start_year = self.start_year
+    end_year = self.end_year
+
     with UseDatabase('weather.sqlite') as cursor:
-      # execute_str = "SELECT avg_temp FROM weather WHERE sample_data LIKE ?",(str(year),)
-      if year == 2020:
-        end_month = datetime.now().month + 1
-      else:
-        end_month = 13
-
-      if year == 1996:
-        start_month = 10
-      else:
-        start_month = 1
-
-      for x in range(start_month,end_month):
-        self.month = x
+      for x in range(1,13):
         monthly_list = []
-        year = str(year)
-        cursor.execute("SELECT avg_temp FROM weather WHERE sample_date LIKE ?", ('%{}%'.format(year +'-'+ str(x).zfill(2)),))
+        for year in range(start_year,end_year + 1):
+          self.month = x
+          year = str(year)
+          cursor.execute("SELECT avg_temp FROM weather WHERE sample_date LIKE ?", ('%{}%'.format(year +'-'+ str(x).zfill(2)),))
 
-        rows = cursor.fetchall()
+          rows = cursor.fetchall()
 
-        for row in rows:
-          if '{}'.format(row[0]) is not '':
-            monthly_list.append(float('{}'.format(row[0])))
-        self.weather_data.update({self.month : monthly_list})
+          for row in rows:
+            if '{}'.format(row[0]) != '':
+              monthly_list.append(float('{}'.format(row[0])))
+          self.weather_data.update({self.month : monthly_list})
 
 
 test = PlotOperations(2000,2017)
 test.create_weather_data()
-print(test.weather_data)
-  # # fake up some data
-  # spread = np.random.rand(50) * 100
-  # center = np.ones(25) * 50
-  # flier_high = np.random.rand(10) * 100 + 100
-  # flier_low = np.random.rand(10) * -100
-  # data = np.concatenate((spread, center, flier_high, flier_low), 0)
+# print(test.weather_data)
+
+fig, ax = plt.subplots()
+ax.boxplot(test.weather_data.values())
+ax.set_xticklabels(test.weather_data.keys())
 
 
-  # # basic plot
-  # plt.boxplot(data)
 
-
-  # # notched plot
-  # plt.figure()
-  # plt.boxplot(data, 1)
-
-  # # change outlier point symbols
-  # plt.figure()
-  # plt.boxplot(data, 0, 'gD')
-
-  # # don't show outlier points
-  # plt.figure()
-  # plt.boxplot(data, 0, '')
-
-  # # horizontal boxes
-  # plt.figure()
-  # plt.boxplot(data, 0, 'rs', 0)
-
-  # # change whisker length
-  # plt.figure()
-  # plt.boxplot(data, 0, 'rs', 0, 0.75)
-
-  # # fake up some more data
-  # # spread = np.random.rand(50) * 100
-  # # center = np.ones(25) * 40
-  # # flier_high = np.random.rand(10) * 100 + 100
-  # # flier_low = np.random.rand(10) * -100
-  # # d2 = np.concatenate((spread, center, flier_high, flier_low), 0)
-  # # data.shape = (-1, 1)
-  # # d2.shape = (-1, 1)
-  # # # data = concatenate( (data, d2), 1 )
-  # # # Making a 2-D array only works if all the columns are the
-  # # # same length.  If they are not, then use a list instead.
-  # # # This is actually more efficient because boxplot converts
-  # # # a 2-D array into a list of vectors internally anyway.
-  # # data = [data, d2, d2[::2, 0]]
-  # # # multiple box plots on one figure
-  # # plt.figure()
-  # plt.boxplot(data)
-
-  # plt.show()
-
-# yn = PlotOperations(1996,2006)
-
-# print(yn.end_year)
+plt.show()
