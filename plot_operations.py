de8237e60b8bbc1131180310cd1d7ff5fd5fac4e
patrickjiang <patrick.jiang86@gmail.com>
2020-04-08 11:58:37 -0500

    single year

--- plot_operations.py
+++ plot_operations.py
@@ -1,33 +1,45 @@
 from db_context_manager import UseDatabase
 import matplotlib.pyplot as plt
 import numpy as np
-
+from datetime import datetime
 
 class PlotOperations():
   def __init__(self,start_year=1996,end_year=2020):
     self.start_year = start_year
     self.end_year = end_year
-    self.weather_data = []
+    self.weather_data = dict()
+    self.month = 0
 
-  def create_weather_data(self,year):
+  def create_weather_data(self,self.start_year,self.end_year):
     with UseDatabase('weather.sqlite') as cursor:
       # execute_str = "SELECT avg_temp FROM weather WHERE sample_data LIKE ?",(str(year),)
-      for x in range(1,13):
+      if year == 2020:
+        end_month = datetime.now().month + 1
+      else:
+        end_month = 13
+
+      if year == 1996:
+        start_month = 10
+      else:
+        start_month = 1
+
+      for x in range(start_month,end_month):
+        self.month = x
         monthly_list = []
         year = str(year)
-        print(x)
         cursor.execute("SELECT avg_temp FROM weather WHERE sample_date LIKE ?", ('%{}%'.format(year +'-'+ str(x).zfill(2)),))
 
         rows = cursor.fetchall()
 
         for row in rows:
-          monthly_list.append(row)
+          if '{}'.format(row[0]) is not '':
+            monthly_list.append(float('{}'.format(row[0])))
+        self.weather_data.update({self.month : monthly_list})
 
 
-
-test = PlotOperations()
-test.create_weather_data(1997)
-print(monthly_list)
+test = PlotOperations(2000,2017)
+test.create_weather_data()
+print(test.weather_data)
   # # fake up some data
   # spread = np.random.rand(50) * 100
   # center = np.ones(25) * 50
